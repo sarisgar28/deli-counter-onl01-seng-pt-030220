6f7ca41e88de1cb2cd9e108f3e5ec6e7c566d017
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-16 16:05:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+def line(deli)
+if deli.length == 0 
+  puts "The line is currently empty."
+else 
+  phrase = "The line is currently:"
+  deli.each_with_index do |name,index|  
+    phrase +=  "#{index + 1 }. #{name}"
+   end 
+  end   
+end
+
+
+def take_a_number(katz_deli,name)
+  if phrase.length == 0 
+    puts "The line is currently:"
+  else 
+    katz_deli_with_name_and_index do |name,index|
+     phrase += "#{index + 1}. #{name}"
+    end
+  end 
+ end  
